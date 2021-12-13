4c9c2ab240ddd2744ace92433ce896eb751c7e76
Adawiyyah Latifah <86681678+adwltfh@users.noreply.github.com>
2021-12-14 01:56:42 +0700

    edit judul

--- application/views/data/tambah_user.php
+++ application/views/data/tambah_user.php
@@ -8,7 +8,7 @@
 		</h1>
 		<ol class="breadcrumb">
 			<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-			<li><a href="#">Data Barang</a></li>
+			<li><a href="#">List User</a></li>
 			<li class="active">Tambah User</li>
 		</ol>
 	</section>
